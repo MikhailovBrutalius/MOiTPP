49934dcd5349ba1498dd2c2769c4cee74d3070f3
MikhailovBrutalius <50700793+MikhailovBrutalius@users.noreply.github.com>
2022-11-14 18:58:54 +0300

    Update lr3.py

--- lr3.py
+++ lr3.py
@@ -3,47 +3,50 @@ from selenium.webdriver.common.by import By
 import webbrowser
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+def getResults():               #функция обнаружения результатов поискового запроса (ссылок) на странице и их записи в список
+    global results_list
+    content = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "b_content")))     
+    results = content.find_elements(By.CLASS_NAME, "b_algo")
+    for result in results:
+        link = result.find_element(By.TAG_NAME, "a").get_attribute("href")
+        results_list.append(link)
+
+def searchAddr(url):            #функция поиска вхождений искомого адреса в тексте html-источника страницы  
+    driver.get(url)
+    body_text = driver.page_source
+    global relevant
+    if body_text.find("Театральная" or "Theatre square" or "Teatralnaya") != -1 :
+        relevant += 1
+        print(url + " - релевантная ссылка")
+    else :
+        print(url)
 
 driver = webdriver.Firefox()
-
+driver.get("https://www.bing.com/")
 relevant = 0
-addr = "Театральная"
-body_text = []
+results_list=[]
 
-def searchAddr():
-    body = driver.find_element_by_tag_name("body")
-    body_text = body.text
-    global relevant
-    if body_text.find(addr) != -1 :
-        relevant += 1
-        #print(relevant)
-        
-driver.get("https://www.google.com/")
-
-search = driver.find_element_by_name('q')
+search = driver.find_element(By.NAME, 'q')
 search.send_keys("Большой театр")
 search.send_keys(Keys.ENTER)
-
 time.sleep(2)
-results_list = []
-results = driver.find_elements_by_class_name('r')
 
-for result in results:
-    print(result.find_element_by_tag_name("a").get_attribute("href"))
-    results_list.append(result.find_element_by_tag_name("a").get_attribute("href"))
+getResults()
 
-time.sleep(2)
-driver.find_element_by_id("pnnext").click();
-    
-results = driver.find_elements_by_class_name('r')
+next_page=WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#b_results > li.b_pag > nav > ul > li:last-child")))
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+next_page.click()
 
-for result in results:
-    print(result.find_element_by_tag_name("a").get_attribute("href"))
-    results_list.append(result.find_element_by_tag_name("a").get_attribute("href"))
+getResults()
 
-for i in range(0, len(results_list)):
-    driver.get(results_list[i])
-    searchAddr()
-    
-print("Релевантных ссылок:")
-print(relevant)
+for result in results_list:
+    searchAddr(result)
+
+print('Релевантных ссылок: ' + str(relevant))
+driver.quit()
+
+
