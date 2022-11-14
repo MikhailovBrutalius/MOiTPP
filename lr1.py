4aa42a49df4826feb30b77f01e4f12c218a791c7
MikhailovBrutalius <50700793+MikhailovBrutalius@users.noreply.github.com>
2022-11-14 18:57:24 +0300

    Update lr1.py

--- lr1.py
+++ lr1.py
@@ -2,34 +2,38 @@ from selenium import webdriver
 import webbrowser
 import random
 import time
-#
-browser = webdriver.Firefox()
 
-f = open('F:\_Outchjoobeescthe\Myotipp\lr1\list.txt', 'r')
+browser = webdriver.Firefox()                               #инициализация браузера
 
-list1 = f.readlines()
-siez = len(list1)
-list2 = []
+file = open('D:\Myotipp\lr1\list.txt', 'r')                 #открытие файла со списком сайтов на чтение 
 
-for i in range(0, siez):
-    rndLine = random.choice(list1)
-    browser.execute_script("window.open('');")
-    browser.switch_to.window(browser.window_handles[-1])
-    browser.get(rndLine)
-    list1.remove(rndLine)
-    list2.append(rndLine)
+list1 = file.readlines()                                    #считывание списка
+size = len(list1)                                           #размер списка 
+list2 = []                                                  #новый список 2 для записи адресов в рандомизированном порядке
 
-f = open('F:\_Outchjoobeescthe\Myotipp\lr1\list.txt', 'w')
+for i in range(0, size):
+    rndLine = random.choice(list1)                          #выбирает случайный сайт из списка
+    browser.execute_script("window.open('');")              #открытие новой вкладки
+    browser.switch_to.window(browser.window_handles[-1])    #переход на новую вкладку (крайнюю справа)
+    browser.get(rndLine)                                    #октрытие сайта на открытой вкладке
+    list1.remove(rndLine)                                   #удаление адреса открытого сайта из списка 1
+    list2.append(rndLine)                                   #занесение адреса в новый список 2 
+
+file = open('D:\Myotipp\lr1\list.txt', 'w')                 #открытие файла со списком сайтов на запись
 time.sleep(1)
 
-for i in range(0,siez):
-    f.write(list2[i])
-    browser.close()
-    browser.switch_to.window(browser.window_handles[-1])
+for i in range(0,size):                                     
+    file.write(list2[i])                                    #запись в файл адреса в новом (случайном) порядке
+    browser.close()                                         #закрытие вкладки
+    browser.switch_to.window(browser.window_handles[-1])    #переход на следующую вкладку (крайнюю справа) 
     time.sleep(1)
       
-f.close()
+file.close()                                                #закрытие файла 
 print("end")
+
+
+
+
 
 
 
